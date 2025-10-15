c94146d127abee812d9b96325b57623acd6859d1
Brandon Breuil <brandon.breuil@infor.com>
2025-10-15 14:26:55 +0000

    Support for ephemeral inputs and write-only attributes

--- internal/jobexecutor/plan.go
+++ internal/jobexecutor/plan.go
@@ -4,13 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"path/filepath"
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclwrite"
-	"github.com/hashicorp/terraform-config-inspect/tfconfig"
 	"github.com/martian-cloud/terraform-exec/tfexec"
-	"github.com/zclconf/go-cty/cty"
 	"gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-api/internal/jobexecutor/jobclient"
 	"gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-api/internal/jobexecutor/joblogger"
 	"gitlab.com/infor-cloud/martian-cloud/tharsis/tharsis-api/pkg/logger"
@@ -39,8 +34,11 @@ func NewPlanHandler(
 	logger logger.Logger,
 	jobLogger joblogger.Logger,
 	client jobclient.Client,
-) *PlanHandler {
-	terraformWorkspace := newTerraformWorkspace(cancellableCtx, jobCfg, workspaceDir, workspace, run, jobLogger, client)
+) (*PlanHandler, error) {
+	terraformWorkspace, err := newTerraformWorkspace(cancellableCtx, jobCfg, workspaceDir, workspace, run, jobLogger, client)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize terraform workspace: %w", err)
+	}
 
 	return &PlanHandler{
 		workspaceDir:       workspaceDir,
@@ -50,7 +48,7 @@ func NewPlanHandler(
 		jobLogger:          jobLogger,
 		client:             client,
 		cancellableCtx:     cancellableCtx,
-	}
+	}, nil
 }
 
 // Cleanup is called after the job has been executed
@@ -103,13 +101,7 @@ func (p *PlanHandler) Execute(ctx context.Context) error {
 	}
 	defer os.RemoveAll(tmpDir)
 
-	// Parse terraform config
-	terraformModule, diag := tfconfig.LoadModule(p.workspaceDir)
-	if diag.HasErrors() {
-		return fmt.Errorf("failed to load terraform module %v", diag)
-	}
-
-	tfVarsFilePath, variablesIncludedInTFConfig, err := p.createVarsFile(terraformModule)
+	tfVarsFilePath, variablesIncludedInTFConfig, err := p.terraformWorkspace.createVarsFile()
 	if err != nil {
 		return fmt.Errorf("failed to process variables: %v", err)
 	}
@@ -218,82 +210,4 @@ func (p *PlanHandler) Execute(ctx context.Context) error {
 	}
 
 	return nil
-}
-
-func (p *PlanHandler) createVarsFile(terraformModule *tfconfig.Module) (string, []string, error) {
-	// Get all variables in the module
-	hclVariables := []types.RunVariable{}
-	stringVariables := []types.RunVariable{}
-	variablesIncludedInTFConfig := []string{}
-
-	for _, v := range p.terraformWorkspace.variables {
-		if v.Category != types.TerraformVariableCategory {
-			continue
-		}
-
-		// Check if there is an hcl definition for this variable
-		variable, ok := terraformModule.Variables[v.Key]
-		if !ok {
-			// Make it easier for the user to identity where the variable is coming from.
-			if v.NamespacePath != nil && *v.NamespacePath == p.terraformWorkspace.workspace.FullPath {
-				p.jobLogger.Warningf("WARNING: Workspace variable %q has a value but is not defined in the terraform module.", v.Key)
-			}
-
-			if v.NamespacePath == nil {
-				p.jobLogger.Warningf("WARNING: Run variable %q has a value but is not defined in the terraform module.", v.Key)
-			}
-
-			continue
-		}
-
-		// Verify that the variable definition is marked as sensitive
-		if v.Sensitive && !variable.Sensitive {
-			return "", nil, fmt.Errorf(
-				"variable %q is marked as sensitive but the hcl definition in the terraform file %q is not sensitive, sensitive variables can only be passed to variable definitions with sensitive set to true",
-				v.Key,
-				filepath.Base(variable.Pos.Filename),
-			)
-		}
-
-		variablesIncludedInTFConfig = append(variablesIncludedInTFConfig, v.Key)
-
-		if isHCLVariable(v.Value, variable) {
-			hclVariables = append(hclVariables, v)
-		} else {
-			stringVariables = append(stringVariables, v)
-		}
-	}
-
-	// First write HCL variables
-	fileContents := ""
-	for _, v := range hclVariables {
-		fileContents += fmt.Sprintf("%s = %s\n", v.Key, *v.Value)
-	}
-
-	// Parse buffer contents
-	f, diag := hclwrite.ParseConfig([]byte(fileContents), "", hcl.InitialPos)
-	if diag != nil {
-		return "", nil, diag
-	}
-	rootBody := f.Body()
-
-	// Use hclwriter for string values to provide HCL character escaping
-	for _, v := range stringVariables {
-		rootBody.SetAttributeValue(v.Key, cty.StringVal(*v.Value))
-	}
-
-	filePath := fmt.Sprintf("%s/run-%s.tfvars", p.workspaceDir, p.run.Metadata.ID)
-	tfVarsFile, err := os.Create(filePath)
-	if err != nil {
-		return "", nil, fmt.Errorf("failed to create temporary file for tfvars %v", err)
-	}
-
-	defer tfVarsFile.Close()
-
-	// Save file
-	if _, err := f.WriteTo(tfVarsFile); err != nil {
-		return "", nil, err
-	}
-
-	return filePath, variablesIncludedInTFConfig, nil
 }
